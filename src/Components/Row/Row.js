fc7a539571532adfd87eee904de8713d17fce6a6
Marcellino <halolino22@gmail.com>
2022-07-11 14:36:24 +0700

    update

--- src/Components/Row/Row.js
+++ src/Components/Row/Row.js
@@ -1,21 +1,46 @@
-import React, { useEffect, useState } from 'react'
-import axios from '../../API/axios'
-import '../../Components/Row/Row.css'
+import React, { useEffect, useState } from 'react';
+import axios from '../../API/axios';
+import '../../Components/Row/Row.css';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 
-const base_url = 'https://image.tmdb.org/t/p/original/'
+const base_url = 'https://image.tmdb.org/t/p/original/';
 
 function Row({ title, fetchUrl, isLargeRow }) {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState('');
 
   //snippet of code which runs based on specific conditions
   useEffect(() => {
     async function fetchData() {
-      const request = await axios.get(fetchUrl)
-      setMovies(request.data.results)
-      return request
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
     }
-    fetchData()
-  }, [fetchUrl])
+    fetchData();
+  }, [fetchUrl]);
+
+  const opst = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl('');
+    } else {
+      movieTrailer(movie.name || '')
+        .then((url) => {
+          // https://www.youtube.com/watch?v=IN5TD4VRcSM
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get('v'));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
 
   return (
     <div className="row">
@@ -25,6 +50,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row_poster ${isLargeRow && 'row_posterLarge'}`}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -33,8 +59,9 @@ function Row({ title, fetchUrl, isLargeRow }) {
           ></img>
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opst} />}
     </div>
-  )
+  );
 }
 
-export default Row
+export default Row;
